33d516c170d623a43f4fb21d9837a982f310834c
Nizam-Mohammed <91694491+Nizam-Mohammed@users.noreply.github.com>
2021-09-30 23:54:19 +0530

    Add files via upload

---
+++ day 1 and day 2/Python zero to hero project day 1 and day 2.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[6]:
+
+
+X=2
+Y=5
+XY="HEY"
+print(X+Y, XY)
+
+
+# In[9]:
+
+
+print("""My name is Rajath Kumar M.P. I love Python.""")
+
+
+# In[10]:
+
+
+String1="World"
+print('Hello',String1)
+
+
+# In[14]:
+
+
+String2='!'
+print('Hello',String1,String2)
+
+
+# In[13]:
+
+
+print('Hello'+ String1+String2)
+
+
+# In[16]:
+
+
+print('Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug')
+
+
+# In[17]:
+
+
+print("I wamt \\n to be printed.")
+
+
+# In[18]:
+
+
+X=10
+print(type(X))
+
+
+# In[19]:
+
+
+s='abc'
+print(type(s))
+
+
+# In[20]:
+
+
+value=True
+print(type(value))
+
+
+# In[22]:
+
+
+percent=90.00
+(type(percent))
+
+
+# In[ ]:
+
+
+
+
